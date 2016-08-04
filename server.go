a1d149fd6fa6bd62b7914f39f14633bd8c5ba4eb
tetafro <tetafro@gmail.com>
2016-08-04 06:42:48 +0300

    Basic server side.

--- server.go
+++ server.go
@@ -5,7 +5,18 @@ import (
     "fmt"
     "net/http"
     "html/template"
+
+    "github.com/gorilla/websocket"
 )
+
+
+var connections map[*websocket.Conn]bool
+
+
+var upgrader = websocket.Upgrader{
+    ReadBufferSize:  1024,
+    WriteBufferSize: 1024,
+}
 
 
 func handlerIndexPage(w http.ResponseWriter, r *http.Request) {
@@ -15,11 +26,43 @@ func handlerIndexPage(w http.ResponseWriter, r *http.Request) {
 }
 
 
+func handlerWS(w http.ResponseWriter, r *http.Request) {
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    fmt.Println("Successfully connected")
+    connections[conn] = true
+
+    _, msg, err := conn.ReadMessage()
+    if err != nil {
+        delete(connections, conn)
+        conn.Close()
+        return
+    }
+    fmt.Println(string(msg))
+    sendAll(msg)
+}
+
+
+func sendAll(msg []byte) {
+    for conn := range connections {
+        err := conn.WriteMessage(websocket.TextMessage, msg)
+        if err != nil {
+            delete(connections, conn)
+            conn.Close()
+        }
+    }
+}
+
+
 func main() {
     port := "8080"
     fmt.Printf("Server is running on %s port...\n", port)
 
     http.HandleFunc("/", handlerIndexPage)
+    http.HandleFunc("/ws", handlerWS)
 
     http.ListenAndServe(":"+port, nil)
 }
